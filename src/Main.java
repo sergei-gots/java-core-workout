de8817f4a807b2c3c1c571ba36911763da25bc6d
Sergei Gots <Serge.Gots@gmail.com>
2023-02-10 20:25:43 +0300

    Online lesson's commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hi there!");
+        System.out.println("This is out within Lesson #01 on Fri 2023/02/10");
 
     }
 }
