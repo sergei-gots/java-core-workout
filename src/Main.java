8ef35f8436a15b86f0f77e5b0968eb0a390db35d
Sergei Gots <Serge.Gots@gmail.com>
2023-02-13 19:54:11 +0300

    Changes to be reverted

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hi there!");
-        System.out.println("This is output made within Lesson #01 on Fri 2023/02/10");
+        System.out.println("Here are changes to be reverted");
 
     }
 }
