4ab7d0506d9ddd98ec7e81bfbb0e7942453c0a9c
Sergei Gots <Serge.Gots@gmail.com>
2023-02-13 20:49:32 +0300

    Template for Homework-1 created

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hi there!");
-        System.out.println("This is out within Lesson #01 on Fri 2023/02/10");
+        System.out.println("Homework - 1");
 
     }
 }
