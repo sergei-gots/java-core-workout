c23a03dfefc97d89c2caf5c005d4cf4b27f16a9c
Sergei Gots <Serge.Gots@gmail.com>
2023-02-14 04:09:20 +0300

    HW-1 Tasks 1-3 completed

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,44 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Homework # 1");
+
+            task1_3();
+    }
+
+    public static void task1_3() {
+
+        System.out.println("Task 1");
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+        System.out.println();
+
+
+        System.out.println("Task 2");
+
+        dog += 4;
+        cat += 4;
+        paper += 4;
+
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+        System.out.println();
+
+        System.out.println("Task 3");
+
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7369;
+
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+        System.out.println();
     }
 }
