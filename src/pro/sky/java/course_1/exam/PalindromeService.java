853953a03ce20eb88d693c706e474c19a245d5e3
Sergei Gots <Serge.Gots@gmail.com>
2023-03-09 04:50:11 +0300

    Exam.ticket's task 'PALINDROME' completed

--- src/pro/sky/java/course_1/exam/PalindromeService.java
+++ src/pro/sky/java/course_1/exam/PalindromeService.java
@@ -8,6 +8,7 @@ public class PalindromeService implements Homework {
     public static final String Panama = "A man, a plan, a canal: Panama!";
     public static final String Panama1 = "A man, a plan, a channel: Panama!";
     public static final String palindromeOfVelimirKhlebnikov = "Мороз в узел, лезу взором.";
+
     public static void main(String[] args) {
         PalindromeService palindromeService = new PalindromeService();
         palindromeService.homework();
@@ -29,22 +30,23 @@ public class PalindromeService implements Homework {
         out.println("So... Have a nice day!:)\n");
 
     }
+
     public void checkAndShowIfItIsPalindrome(String exp) {
         boolean isPalindrome = isPalindrome(exp);
         out.println("The expression \"" + exp +
-                ((isPalindrome) ? " is a palindrome!:)\n" : " is not a palindrome:(\n"));
+                ((isPalindrome) ? "\" is a palindrome!:)\n" : "\" is not a palindrome:(\n"));
     }
 
     public boolean isPalindrome(String exp) {
         out.println("Now we check if an expression is palindrome or not.");
         out.println("The expression is: \"" + exp + "\".");
-        String reducedExp = exp.replaceAll("(\\s+)|([.,!?:;'\\\"\\'-])", "").toLowerCase();
+        String reducedExp = exp.replaceAll("(\\s+)|([.,!?:;'\"\\'-])", "").toLowerCase();
         out.println("The expression after removing spaces and punctuation and reduced to lower case: \"" +
-                reducedExp    + "\".");
+                reducedExp + "\".");
         if (reducedExp.length() < 2) {
             return true;
         }
-        for (int i = 0; i <= reducedExp.length()/2 + reducedExp.length()%2 ; i++) {
+        for (int i = 0; i <= reducedExp.length() / 2 + reducedExp.length() % 2; i++) {
             if (reducedExp.charAt(i) != reducedExp.charAt(reducedExp.length() - 1 - i)) {
                 return false;
             }
