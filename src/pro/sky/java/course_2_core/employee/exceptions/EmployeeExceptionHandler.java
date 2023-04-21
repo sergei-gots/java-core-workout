e5526d477368b02075964297585147c10d65b563
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-04-22 02:27:55 +0300

    hw-2.9 "Libraries" completed

--- src/pro/sky/java/course_2_core/employee/exceptions/EmployeeExceptionHandler.java
+++ src/pro/sky/java/course_2_core/employee/exceptions/EmployeeExceptionHandler.java
@@ -20,4 +20,11 @@ public class EmployeeExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
+    @ExceptionHandler(IllegalNameException.class)
+    public ResponseEntity<String> illegalNameExceptionHandler(@NotNull IllegalNameException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
+
+    }
+
 }
