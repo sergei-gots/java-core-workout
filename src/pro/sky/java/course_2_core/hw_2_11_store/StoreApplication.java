9c1734fe10bf73a47e8cb29e78d37cf1063ff04e
Sergei Gots <Serge.Gots@gmail.com>
2023-04-27 15:43:08 +0300

    StoreApplication corrected

--- src/pro/sky/java/course_2_core/hw_2_11_store/StoreApplication.java
+++ src/pro/sky/java/course_2_core/hw_2_11_store/StoreApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class storeApplication {
+public class StoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(storeApplication.class);
+        SpringApplication.run(StoreApplication.class);
     }
 }
