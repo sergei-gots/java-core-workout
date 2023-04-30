f87215721b5f2a358a63455035d0c21acae15b47
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-04-30 18:16:42 +0300

    hw-2.12 "Calculator. Unit Tests" initial commit

---
+++ hw-2-12-Calculator-Unit-Testing/src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -0,0 +1,44 @@
+package pro.sky.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.calculator.service.CalculatorServiceInterface;
+import pro.sky.calculator.service.CalculatorServiceInterface;
+
+@RestController
+@RequestMapping(path = "/calculator")
+public class CalculatorController {
+    private final CalculatorServiceInterface calculatorService;
+
+    public CalculatorController(CalculatorServiceInterface calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String welcome() {
+        return calculatorService.welcome();
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return calculatorService.plus(a, b);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return calculatorService.minus(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return calculatorService.divide(a, b);
+    }
+
+}
